9287d5589a62a32bc462413a141431ec27fa91a2
Rasmus Nielsen <rasni@itu.dk>
2022-09-07 17:07:30 +0200

    SpawnFork

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
-import (
-	"fmt"
-)
+import "sync"
+
+type ForkMsg struct {
+	message  string
+	response bool
+}
+
+func spawnFork(ch chan ForkMsg) {
+	isClean := false
+	msg := <-ch
+	switch msg.message {
+	case "isClean":
+		ch <- ForkMsg{"isClean", isClean}
+	case "setIsClean":
+		isClean = msg.response
+		ch <- ForkMsg{"isClean", isClean}
+	}
+
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	n := 5
+
+	wg := new(sync.WaitGroup)
+
+	forkChans := make([]chan ForkMsg, n)
+	philChans := make([]chan string, n)
+
+	for i := range forkChans {
+		forkChans[i] = make(chan ForkMsg)
+	}
+
+	for i := range forkChans {
+		philChans[i] = make(chan string)
+	}
+
+	wg.Wait()
+
 }
